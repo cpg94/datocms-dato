bb1bed3dd49cce84e9bee41a331a13ea84dceaf5
Chris Golding <chris.golding@korelogic.co.uk>
2020-02-27 09:10:18 +0000

    added h1 to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,11 @@ import Sidebar from '../components/Sidebar'
 import '../styles/index.sass'
 // import Img from 'gatsby-image'
 
-const IndexPage = ({ data }) => console.log({ data }) || (
+const IndexPage = ({ data }) => (
   <Background>
     <Sidebar />
+    <h1 style={{ fontWeight: 'bolder', color: 'red', fontSize: '80px'}}>Index</h1>
+
   </Background>
 )
 
